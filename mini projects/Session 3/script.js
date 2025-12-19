b19096f7087bd18b297d71b389f70c6c45d2bbeb
Abdi Dabala <abdid.yadata@gmail.com>
2025-12-19 21:14:44 +0300

    Toggle Text

--- mini projects/Session 3/script.js
+++ mini projects/Session 3/script.js
@@ -1,11 +1,11 @@
-// 1. Select the elements
+// 1. select the elements
 const statusLabel = document.getElementById('statusLabel');
 const toggleBtn = document.getElementById('toggleBtn');
 
-// 2. Add an event listener for the click action
+// 2. add an event listener for the click action
 toggleBtn.addEventListener('click', function() {
     
-    // 3. Logic: Check current state and toggle
+    // 3. logic: Check current state and toggle
     if (statusLabel.textContent === 'OFF') {
         statusLabel.textContent = 'ON';
         statusLabel.style.color = '#28a745'; // Optional: Change color to green
